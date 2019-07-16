9ab92cc44394989bd04bd2985fcd840cd41d5fc5
Zack Li <zequnl@tigercpu.princeton.edu>
2019-07-16 15:28:53 -0400

    added setup.py for emilie

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nawrapper',
+      version='0.1',
+      description='wrapper around namaster for diagnostic spectra',
+      url='http://github.com/xzackli/nawrapper',
+      author='Zack Li',
+      author_email='zequnl@astro.princeton.edu',
+      license='MIT',
+      packages=['nawrapper'],
+      zip_safe=False)
