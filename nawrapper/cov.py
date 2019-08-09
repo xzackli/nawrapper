d96d5e1e8ac053037292a902a578364c9e3dc85b
xzackli <xzackli@gmail.com>
2019-08-08 21:59:15 -0400

    added DIY mask routines to daytime notebook

--- nawrapper/cov.py
+++ nawrapper/cov.py
@@ -7,43 +7,6 @@ from pixell import enmap
 import nawrapper.maputils as maputils
 
 
-def compute_cov(namap1, namap2, bins=None, mc=None):
-    r"""Compute all of the covariance matrices between two maps.
-
-    This computes all cross covariances between two :py:class:`nawrapper.ps.namap`.
-    If both input namap objects have polarization information, then polarization
-    covariances will be computed. In all cases, TT is computed.
-
-    Parameters
-    ----------
-    namap1 : :py:class:`nawrapper.ps.namap` object.
-        The first map to compute correlations with.
-    namap2 : :py:class:`nawrapper.ps.namap` object.
-        To be correlated with `namap1`.
-    bins : NaMaster NmtBin object (optional)
-        At least one of `bins` or `mc` must be specified. If you specify
-        `bins` (possibly from the output of :py:func:`nawrapper.ps.read_bins`)
-        then a new mode coupling matrix will be computed within this function
-        call. If you have already computed a relevant mode-coupling matrix,
-        then pass `mc` instead.
-    mc : :py:class:`nawrapper.ps.mode_coupling` object (optional)
-        This object contains precomputed mode-coupling matrices.
-
-    Returns
-    -------
-    Cb : dictionary
-        Binned spectra, with the relevant cross spectra (i.e. 'TT', 'TE', 'EE')
-        as dictionary keys. This also contains the bin centers as key 'ell'.
-
-    """
-    cw = nmt.NmtCovarianceWorkspace()
-    # This is the time-consuming operation
-    # Note that you only need to do this once,
-    # regardless of spin
-    cw.compute_coupling_coefficients(f1t, f2t, f1t, f2t, lmax=lmax)
-    
-
-
 class nacov:
     r"""Wrapper around the NaMaster covariance workspace object.
 
@@ -55,6 +18,8 @@ class nacov:
         r"""
         Create a `nacov` object.
 
+        Parameters
+        ----------
         namap1: namap
             We use the mask in this namap to compute the mode-coupling matrices.
         namap2: namap
@@ -73,18 +38,21 @@ class nacov:
             lmax=self.lmax)
 
         covar_00_00 = nmt.gaussian_covariance(
-            cw,
+            self.cw,
             0, 0, 0, 0,  # Spins of the 4 fields
-            [clth + get_Nl(l_max=2509)], 
-            [clth],  # TT
-            [clth],  # TT
-            [clth + get_Nl(l_max=2509)],  # TT
+            [theory['T1T1']],
+            [theory['T1T2']],
+            [theory['T2T1']],
+            [theory['T2T2']],
             mc.w00, wb=mc.w00)
+        ct = covar_00_00.reshape(
+            [self.lmax-1, 1, self.lmax-1, 1])
+        self.covT1T2T1T2 = ct[:, 0, :, 0]
         
         
 def get_Nl(theta_fwhm=(10., 7., 5.),
              sigma_T=(68.1, 42.6, 65.4),
-             f_sky=0.6, l_min=2, l_max=2500,
+             f_sky=0.6, l_min=2, l_max=2509,
              verbose=False):
     """
     Get Knox TT noise curve.
@@ -92,13 +60,21 @@ def get_Nl(theta_fwhm=(10., 7., 5.),
     
     Parameters
     ----------
-        theta_fwhm (list of float): beam resolution in arcmin
-        sigma_T (list of float): temperature resolution in muK
-        sigma_P (list of float): polarization resolution in muK
-        f_sky (float): sky fraction covered
-        l_min (int): minimum ell for CMB power spectrum
-        l_max (int): maximum ell for CMB power spectrum
-        verbose (boolean): flag for printing out debugging output
+        theta_fwhm : list of float: 
+            beam resolution in arcmin
+        sigma_T : list of float
+            temperature resolution in muK
+        sigma_P : list of float
+            polarization resolution in muK
+        f_sky : float
+            sky fraction covered
+        l_min : int
+            minimum ell for CMB power spectrum
+        l_max : int
+            maximum ell for CMB power spectrum
+        verbose : bool
+            flag for printing out debugging output
+
     """
 
     # convert from arcmin to radians
