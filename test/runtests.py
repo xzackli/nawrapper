23bc619031fc8f95fe6a9e1196aa71ba949521e4
xzackli <xzackli@gmail.com>
2019-10-21 13:55:54 -0400

    configuring codecov to see the package

--- test/runtests.py
+++ test/runtests.py
@@ -1,10 +1,14 @@
 import unittest
+import numpy as np
 import nawrapper as nw
 
 class NaWrapperTest(unittest.TestCase):
 
     def test_basic(self):
         assert 1 == 1
+
+        b = nw.get_unbinned_bins(10) 
+        assert np.all(b.get_effective_ells() == np.arange(2,11).astype(float))
 
 
 if __name__ == '__main__':
